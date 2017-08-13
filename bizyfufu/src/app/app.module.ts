84f3c843597e7d802aa2f310d739efd9d5ed350e
Zakarya Ali <zakarya.ali@gmail.com>
2017-08-13 18:04:34 +0200

    Webapp : Ajout Footer (commenté)

--- bizyfufu/src/app/app.module.ts
+++ bizyfufu/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PlatSearchComponent } from './plat-search.component';
 import { PlatService } from './plat.service';
 import { AdressSearchComponent } from './adress-search.component';
 import { MapComponent } from './map.component';
+import { FooterComponent } from './footer.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -31,7 +32,8 @@ import { InMemoryDataService }  from './in-memory-data.service';
     RestaurantDetailComponent,
     PlatSearchComponent,
     AdressSearchComponent,
-    MapComponent
+    MapComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
