fb1f3ed0bb6d42c46ef3ecdd781447b382941d69
Zakarya Ali <zakarya.ali@gmail.com>
2018-01-02 16:53:43 +0100

    Neff : ajout restaurants - service et class

---
+++ neffMobile/src/app/restaurant.ts
@@ -0,0 +1,10 @@
+export class Restaurant {
+  id: number;
+  logo: string;
+  name: string;
+  address:  string;
+  specialties: string[];
+  work_time: string;
+  minimum_order: string;
+  link: string;
+}
