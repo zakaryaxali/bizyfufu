b53a881fe225f741fb93970c9ff4cbedb272ad40
Zakarya Ali <zakarya.ali@gmail.com>
2018-01-02 16:54:09 +0100

    Neff : modif bouchon meals

--- neffMobile/src/app/utils/in-memory-data.service.ts
+++ neffMobile/src/app/utils/in-memory-data.service.ts
@@ -1,11 +1,11 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import * as mockRestaurants from '../../assets/data/restaurants.json';
-import * as mockPlats from '../../assets/data/plats.json';
+import * as mockMeals from '../../assets/data/meals.json';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const restaurants = <any>mockRestaurants;
-    const plats = <any>mockPlats;
-    return {plats, restaurants};
+    const meals = <any>mockMeals;
+    return {meals, restaurants};
   }
 }
