b2816a1e88ad2828e252ac7800c53ed72e5182eb
Zakarya Ali <zakarya.ali@gmail.com>
2018-01-02 16:54:44 +0100

    Neff : Ajout meal-detail

--- neffMobile/src/app/meal-detail/meal-detail.component.ts
+++ neffMobile/src/app/meal-detail/meal-detail.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import 'rxjs/add/operator/switchMap';
+
+import { Meal } from '../meal';
+import { MealService } from '../meal.service';
+import { Restaurant } from '../restaurant';
+import { RestaurantService } from '../restaurant.service';
 
 @Component({
   selector: 'app-meal-detail',
   templateUrl: './meal-detail.component.html',
-  styleUrls: ['./meal-detail.component.css']
+  styleUrls: ['./meal-detail.component.css'],
+  providers: [MealService, RestaurantService]
 })
 export class MealDetailComponent implements OnInit {
+  meal: Meal;
+  restaurant: Restaurant;
 
-  constructor() { }
+  constructor(
+    private mealService: MealService,
+    private restaurantService: RestaurantService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    var a = this;
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.mealService.getMeal(+params.get('id')))
+      .subscribe(meal => {
+        a.meal = meal;
+        // a.getRestaurant(a.meal);
+      });
   }
+
+    getRestaurant(meal: Meal): void {
+      var a = this;
+      this.restaurantService.getRestaurant(meal.id_restaurant).then(restaurant => {
+        a.restaurant = restaurant;
+      });
+    }
+
+    goBack(): void {
+      this.location.back();
+    }
 
 }
