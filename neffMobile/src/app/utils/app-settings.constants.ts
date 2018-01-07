dc204f73542aa6c08816d20cfef2e7af96541c24
Zakarya Ali <zakarya.ali@gmail.com>
2018-01-07 11:04:13 +0100

    Neff : ajout images plats

---
+++ neffMobile/src/app/utils/app-settings.constants.ts
@@ -0,0 +1,27 @@
+export class AppSettings {
+  public static  dir = "../../assets/img/food";
+  public static MEALS_IMAGES=[
+    "asparagus.jpg",
+    "breakfast.jpg",
+    "chicken.jpg",
+    "corn-bread.jpg",
+    "eating.jpg",
+    "fish-and-chip.jpg",
+    "food.jpg",
+    "food1.jpg",
+    "food2.jpg",
+    "food3.jpg",
+    "fried.jpg",
+    "hamburger.jpg",
+    "noodles.jpg",
+    "pancakes.jpg",
+    "pizza.jpg",
+    "pumpkin.jpg",
+    "pumpkin-soup.jpg",
+    "salad.jpg",
+    "salmon.jpg",
+    "smoked-beef.jpg",
+    "tarte.jpg",
+    "yogurt.jpg"
+  ]
+}
